fd35cd2e20223a009bd029fa5c9691a32df0a457
Omar <Omanas_98@hotmail.com>
2019-11-07 12:17:39 +0100

    Career sidan klar, javascript tillagd till formen

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
+
 $(".dropdown-toggle").dropdown();
+
+
+//Start of form JS ------
+//Checks if invalids form fields are invalid
+(function() {
+	'use strict';
+	window.addEventListener('load', function() {
+		// Catch all the forms we want to apply custom Bootstrap validation styles to
+		var forms = document.getElementsByClassName('needs-validation');
+		// Loop over them and prevent submission
+		var validation = Array.prototype.filter.call(forms, function(form) {
+			form.addEventListener('submit', function(event) {
+				if (form.checkValidity() === false) {
+					event.preventDefault();
+					event.stopPropagation();
+				}
+				form.classList.add('was-validated');
+			}, false);
+		});
+	}, false);
+})();
+//End of form JS -----
